30b7b4333e1dcfd3f27e4ed982146bd39524761c
Mbzneto <72531253+Mbzneto@users.noreply.github.com>
2023-07-09 17:37:20 -0300

    Botões de movimento na tela com highlight quando pressionados

---
+++ Assets/Scripts/HighlitTeclaUI.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class HighlitTeclaUI : MonoBehaviour
+{
+    public Sprite normal, highlight;
+    private Image image;
+    PlayerInputActions playerInputActions;
+    public string tecla;
+
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    void OnEnable()
+    {
+        playerInputActions.Player.Movimentacao.performed += TeclaPressionada;
+        playerInputActions.Player.Movimentacao.canceled += TeclaSolta;     
+    }
+
+    void OnDisable()
+    {
+        playerInputActions.Player.Movimentacao.performed -= TeclaPressionada;
+        playerInputActions.Player.Movimentacao.canceled -= TeclaSolta;
+    }
+
+    void Start()
+    {
+        image = this.GetComponent<Image>();
+        image.sprite = normal;
+    }
+
+    public void TeclaPressionada(InputAction.CallbackContext context)
+    {
+        if ("Key:/Keyboard/" + tecla == context.control.ToString())
+        {
+            image.sprite = highlight;
+        }       
+    }
+
+    public void TeclaSolta(InputAction.CallbackContext context)
+    {
+        if ("Key:/Keyboard/" + tecla == context.control.ToString())
+        {
+            image.sprite = normal;
+        }
+    }
+}
