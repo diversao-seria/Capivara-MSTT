77ca9233ffc69f051711184bf973e06f3f902887
Wictkyng <ryansallatti@gmail.com>
2023-08-14 11:50:37 -0300

    Sistema_Guardião

---
+++ Assets/Scripts/Controllers/Guardioes_controler.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guardioes_controler : MonoBehaviour
+
+{
+    [SerializeField] private char notaMexe;
+    [SerializeField] private ParticleSystem sistema_particulas;
+    [SerializeField] private Sprite spriteO, spriteI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (notaMexe == 'I') {
+            sistema_particulas.textureSheetAnimation.SetSprite(0, spriteI);
+        }
+
+        else if (notaMexe == 'O')
+        {
+            sistema_particulas.textureSheetAnimation.SetSprite(0, spriteO);
+        }
+    }
+    void OnEnable()
+
+    {
+        GridController.NotePlayed += Guardioes_mexe;
+    }
+    void OnDisable()
+    {
+        GridController.NotePlayed -= Guardioes_mexe;
+    }
+
+
+    // Update is called once per frame
+    void Guardioes_mexe(char note)
+    {
+        if (note == notaMexe)
+        {
+           sistema_particulas.Play();
+        }
+    }
+}
