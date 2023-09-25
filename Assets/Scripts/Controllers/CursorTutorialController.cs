588db46e2c5cf7373daea58dafc8ef2a5dfdd168
Mbzneto <72531253+Mbzneto@users.noreply.github.com>
2023-09-25 14:51:49 -0300

    Cursor guia funcionando

--- Assets/Scripts/Controllers/CursorTutorialController.cs
+++ Assets/Scripts/Controllers/CursorTutorialController.cs
@@ -8,23 +8,23 @@ public class CursorTutorialController : MonoBehaviour
 {
     [SerializeField] private List<Transform> posicoesFinais = new List<Transform>();
     [SerializeField] private Sprite spriteClique, spritePadrao;
-    [SerializeField] private float tempoDeAnimacao = 0.5f, esperaCursor = 5f;
+    [SerializeField] private float tempoDeAnimacao = 0.5f, esperaCursor = 2f;
     [SerializeField] private AnimationCurveReference animCurve;
     [SerializeField] private Image componenteImagem;
+    [SerializeField] private GameObject cursorTutorialPrefab;
     private bool spriteAtualClique = false;
-    private bool timerRodando = true;
+    private bool timerRodando = false;
     private float tempoAtual;
     private int posicaoAtual = 0;
+    private bool cursorPiscando = false; 
 
     public void SomTerminou()
     {
-
-        // teste de commit no git do lab
-
         if (posicoesFinais.Count > 0)
         {
-            // StartCoroutine(TimerTutorial());
-            Debug.Log("Sicke :P");
+            tempoAtual = esperaCursor;
+            timerRodando = true;
+            posicaoAtual = 0;
         } 
     }
 
@@ -36,7 +36,7 @@ public class CursorTutorialController : MonoBehaviour
         {
             if (posicaoAtual < posicoesFinais.Count)
             {
-                StartCoroutine(MoveCursor());
+                StartCoroutine(ControladorAnimacaoCursor());
             }
             else
             {
@@ -45,7 +45,6 @@ public class CursorTutorialController : MonoBehaviour
         }
         else
         {
-            // decidir se devo continuar ou interromper o tutorial após o clique
             posicaoAtual++;
             tempoAtual = esperaCursor;
         }    
@@ -53,12 +52,21 @@ public class CursorTutorialController : MonoBehaviour
 
     public void ErrouMSTT()
     {
+        StopAllCoroutines(); 
+
         componenteImagem.enabled = false;
-        StopAllCoroutines();
-        posicaoAtual = 0;
-        tempoAtual = esperaCursor;
+        cursorPiscando = false;
+
+        tempoAtual = esperaCursor;  
+        
         timerRodando = true;
-        Debug.Log(posicaoAtual);
+    }
+
+    public void MSTTAcerto()
+    {
+        componenteImagem.enabled = false;
+        cursorPiscando = false;
+        timerRodando = false;
     }
 
 
@@ -76,23 +84,28 @@ public class CursorTutorialController : MonoBehaviour
             }
             else
             {
+                Debug.Log(posicaoAtual);
                 componenteImagem.enabled = true;
                 timerRodando = false;
-                StartCoroutine(MoveCursor());
+                cursorPiscando = true;
+                StartCoroutine(ControladorAnimacaoCursor());
             }
         }         
     }
 
-    // TODO: TROCAR A CORROTINA POR UM TIMER PRA EVITAR INTERRUPÇÕES E MANTER O TUTORIAL ATIVO MESMO SE O JOGADOR CLICAR EM ALGUM BOTÃO ANTES DO TÉRMINO DO TIMER
-    IEnumerator TimerTutorial()
+    IEnumerator ControladorAnimacaoCursor()
     {
-        yield return new WaitForSeconds(esperaCursor);
-        componenteImagem.enabled = true;
-        StartCoroutine(MoveCursor());
+        yield return MoveCursor();
+
+        posicaoAtual++;
+
+        yield return new WaitForSeconds(0.3f);
+
+        yield return PiscarCursor();
     }
 
     IEnumerator MoveCursor()
-    {     
+    {   
         spriteAtualClique = false;
         componenteImagem.sprite = spritePadrao;
 
@@ -108,32 +121,12 @@ public class CursorTutorialController : MonoBehaviour
             transform.position = posicaoIntermediaria;
             tempoPassado += Time.deltaTime;
             yield return null;
-        }
-        posicaoAtual++;
-        
-        yield return new WaitForSeconds(0.3f);
-        
-
-        while (true)
-        {           
-            if (spriteAtualClique)
-            {
-                componenteImagem.sprite = spritePadrao;
-            }
-            else
-            {
-                componenteImagem.sprite = spriteClique;
-            }
-            spriteAtualClique = !spriteAtualClique;
-            yield return new WaitForSeconds(1f);
-        }
-
-        // StartCoroutine(PiscarCursor());      
+        }        
     }
 
     IEnumerator PiscarCursor()
     {
-        while (true)
+        while (cursorPiscando)
         {           
             if (spriteAtualClique)
             {
